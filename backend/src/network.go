0ee2bb6401ee42ee58f3c2e0957517cbe9adecf3
Jakob Bauers <me@jbauers.com>
2020-10-09 11:54:28 +0200

    Properly increment IPs

--- backend/src/network.go
+++ backend/src/network.go
@@ -4,36 +4,24 @@ import (
 	"net"
 )
 
-// Get all valid hosts in a subnet.
-func hosts(cidr string) ([]string, error) {
-	ip, ipnet, err := net.ParseCIDR(cidr)
-	if err != nil {
-		return nil, err
-	}
-
-	var ips []string
-	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); iterateHosts(ip) {
-		ips = append(ips, ip.String())
-	}
-
-	// Remove network address and broadcast address.
-	lenIPs := len(ips)
-	switch {
-	case lenIPs < 2:
-		return ips, nil
-
-	default:
-		return ips[1 : len(ips)-1], nil
-	}
-}
-
-func iterateHosts(ip net.IP) {
+func incrementIP(currIP string) (newIP string) {
+	ip := net.ParseIP(currIP)
 	for j := len(ip) - 1; j >= 0; j-- {
 		ip[j]++
 		if ip[j] > 0 {
 			break
 		}
 	}
+	return ip.String()
+}
+
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
 // Get the allowed IP. This may only be a /32 (for IPv4).
--- backend/src/redis.go
+++ backend/src/redis.go
@@ -22,14 +22,15 @@ func handleClient(uid string) (ip string) {
 	val, err := rc.Get(uid).Result()
 
 	if err == redis.Nil || val == "" {
-		// Get a random IP from the available IP pool.
-		// FIXME: Performance on a /16 network. Don't even dare /8.
-		ip, err = rc.SRandMember("availableIPs").Result()
+		res, err := rc.SMembers("usedIPs").Result()
 		check(err)
 
-		// Remove the assigned IP from the pool.
-		err = rc.SRem("availableIPs", ip).Err()
-		check(err)
+		for _, r := range res {
+			ip = incrementIP(r)
+			for stringInSlice(ip, res) {
+				ip = incrementIP(ip)
+			}
+		}
 
 		// Add them to the used IP pool.
 		err = rc.SAdd("usedIPs", ip).Err()
@@ -106,15 +107,8 @@ func getPeerList() (peerList []wgtypes.PeerConfig) {
 func initServer() (privkey string, pubkey string) {
 	rc := redisClient()
 
-	// This adds all hosts in our subnet.
-	// FIXME: RIP on a /8, unsurprisingly. Maybe it's better to
-	// iterate 1 by 1 and keep track elsewhere.
-	availableIPs, err := hosts(serverCIDR)
-	err = rc.SAdd("availableIPs", availableIPs).Err()
-	check(err)
-
 	// Remove the server IP from the available IP pool.
-	err = rc.SRem("availableIPs", serverIP).Err()
+	err := rc.SAdd("usedIPs", serverIP).Err()
 	check(err)
 
 	// Add the server configuration to Redis.
